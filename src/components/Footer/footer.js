8f2399cb55765a415c5ed0d7d685d663388c0fb9
batoolalali <abdullahbatool93@gmail.com>
2020-06-28 17:48:14 +0300

    finished lab 26

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import './footer.scss'
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="copyRight"> © 2020 Code Fellow</p>
+    </div>
+  );
+};
+
+export default Footer;
