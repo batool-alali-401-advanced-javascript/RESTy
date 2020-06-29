b0d8adaf57b103a9a057bbc235328372aefa0236
batoolalali <runner@fv-az70.5aukcpqfdbuuzj5y50cj2pei1e.bx.internal.cloudapp.net>
2020-06-29 13:49:41 +0000

    Updates

---
+++ precache-manifest.0feb4458a46e12dd083eebc231688720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b3467e4abcfde99ed476e029e590f5",
+    "url": "/RESTy/index.html"
+  },
+  {
+    "revision": "67937d47b848a4e9c736",
+    "url": "/RESTy/static/css/main.6912df11.chunk.css"
+  },
+  {
+    "revision": "3c3fe42eb5bed02bd8ed",
+    "url": "/RESTy/static/js/2.62d548b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RESTy/static/js/2.62d548b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67937d47b848a4e9c736",
+    "url": "/RESTy/static/js/main.562049a3.chunk.js"
+  },
+  {
+    "revision": "cffb6c70ef4be7c07bfe",
+    "url": "/RESTy/static/js/runtime-main.8243ec0e.js"
+  }
+]);
